82d90a77c79fff9e9f0d5a09f369ca5f6e3310ea
kSibalic <sibalickarlo@gmail.com>
2024-03-07 11:55:10 +0100

    Initial commit

---
+++ lv1.py
@@ -0,0 +1,25 @@
+#primjer upotrebe aritmetičkog operatora
+
+x = 23
+print(x)
+x = x+7
+print(x)
+
+#primjer lista
+lstEmpty = []
+lstFriend = ['Marko', 'Luka', 'Pero']
+
+lstFriend.append('Ivan')
+
+print(lstFriend[0])
+
+print(lstFriend[0:1:2])
+print(lstFriend[:2])
+print(lstFriend[1:])
+print(lstFriend[1:3])
+
+#primjer lista
+a = [1, 2, 3]
+b = [4, 5, 6]
+c = a+b
+print(c)

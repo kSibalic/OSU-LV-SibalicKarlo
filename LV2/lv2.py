2ae167d8587fe4a9b37c46a2f8de96c3994fee21
kSibalic <sibalickarlo@gmail.com>
2024-03-14 11:59:55 +0100

    Add materials to LV2 folder

--- LV2/lv2.py
+++ LV2/lv2.py
@@ -1,1 +1,13 @@
-print("OSULV")
+#pip install numpy
+#pip install matplotlib
+
+import numpy as np
+
+a = np.array([6, 2, 9])
+
+print(type(a))
+print(a.shape)
+print(a[0], a[1], a[2])
+
+a[1] = 5
+print(a)

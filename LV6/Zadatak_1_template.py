64bf91f673c39d974f399dc939786e782a877bf9
kSibalic <sibalickarlo@gmail.com>
2024-04-11 09:53:54 +0200

    Fulfilled Zadatak_1_template.py file

--- LV6/Zadatak_1_template.py
+++ LV6/Zadatak_1_template.py
@@ -14,24 +14,25 @@ from sklearn.pipeline import Pipeline
 from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import GridSearchCV
 
+
 def plot_decision_regions(X, y, classifier, resolution=0.02):
     plt.figure()
     # setup marker generator and color map
     markers = ('s', 'x', 'o', '^', 'v')
     colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
     cmap = ListedColormap(colors[:len(np.unique(y))])
-    
+
     # plot the decision surface
     x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
     xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
-    np.arange(x2_min, x2_max, resolution))
+                           np.arange(x2_min, x2_max, resolution))
     Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
     Z = Z.reshape(xx1.shape)
     plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
     plt.xlim(xx1.min(), xx1.max())
     plt.ylim(xx2.min(), xx2.max())
-    
+
     # plot class examples
     for idx, cl in enumerate(np.unique(y)):
         plt.scatter(x=X[y == cl, 0],
@@ -50,11 +51,11 @@ data.hist()
 plt.show()
 
 # dataframe u numpy
-X = data[["Age","EstimatedSalary"]].to_numpy()
+X = data[["Age", "EstimatedSalary"]].to_numpy()
 y = data["Purchased"].to_numpy()
 
 # podijeli podatke u omjeru 80-20%
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, stratify=y, random_state = 10)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=10)
 
 # skaliraj ulazne velicine
 sc = StandardScaler()
@@ -62,16 +63,16 @@ X_train_n = sc.fit_transform(X_train)
 X_test_n = sc.transform((X_test))
 
 # Model logisticke regresije
-LogReg_model = LogisticRegression(penalty=None) 
+LogReg_model = LogisticRegression(penalty=None)
 LogReg_model.fit(X_train_n, y_train)
 
 # Evaluacija modela logisticke regresije
 y_train_p = LogReg_model.predict(X_train_n)
 y_test_p = LogReg_model.predict(X_test_n)
 
-print("Logisticka regresija: ")
-print("Tocnost train: " + "{:0.3f}".format((accuracy_score(y_train, y_train_p))))
-print("Tocnost test: " + "{:0.3f}".format((accuracy_score(y_test, y_test_p))))
+print("Logistička regresija: ")
+print("Točnost train: " + "{:0.4f}".format((accuracy_score(y_train, y_train_p))))
+print("Točnost test: " + "{:0.4f}".format((accuracy_score(y_test, y_test_p))))
 
 # granica odluke pomocu logisticke regresije
 plot_decision_regions(X_train_n, y_train, classifier=LogReg_model)
@@ -81,4 +82,114 @@ plt.legend(loc='upper left')
 plt.title("Tocnost: " + "{:0.3f}".format((accuracy_score(y_train, y_train_p))))
 plt.tight_layout()
 plt.show()
+
+# Zadatak 1.
+# a)
+KNN_model = KNeighborsClassifier(n_neighbors=5)
+KNN_model.fit(X_train, y_train)
+
+y_test_p_KNN = KNN_model.predict(X_test_n)
+y_train_p_KNN = KNN_model.predict(X_train_n)
+
+print("Točnost za K=5 - testiranje:", accuracy_score(y_test, y_test_p_KNN))
+print("Točnost za K=5 - učenje:", accuracy_score(y_train, y_train_p_KNN))
+
+plot_decision_regions(X_train_n, y_train, classifier=KNN_model)
+plt.xlabel('x_1')
+plt.ylabel('x_2')
+plt.legend(loc='upper left')
+plt.title("Točnost: " + "{:0.4f}".format((accuracy_score(y_train, y_train_p_KNN))))
+plt.tight_layout()
+plt.show()
+
+# b)
+KNN_model = KNeighborsClassifier(n_neighbors=1)
+KNN_model.fit(X_train_n, y_train)
+
+y_test_p_KNN = KNN_model.predict(X_test_n)
+y_train_p_KNN = KNN_model.predict(X_train_n)
+
+print("Točnost za K=1 - testiranje:", accuracy_score(y_test, y_test_p_KNN))
+print("Točnost za K=1 - učenje:", accuracy_score(y_train, y_train_p_KNN))
+
+plot_decision_regions(X_train_n, y_train, classifier=KNN_model)
+plt.xlabel('x_1')
+plt.ylabel('x_2')
+plt.legend(loc='upper left')
+plt.title("Točnost: " + "{:0.4f}".format((accuracy_score(y_train, y_train_p_KNN))))
+plt.tight_layout()
+plt.show()
+
+KNN_model = KNeighborsClassifier(n_neighbors=100)
+KNN_model.fit(X_train_n, y_train)
+
+y_test_p_KNN = KNN_model.predict(X_test_n)
+y_train_p_KNN = KNN_model.predict(X_train_n)
+
+print("Točnost za K=100 - testiranje:", accuracy_score(y_test, y_test_p_KNN))
+print("Točnost za K=100 - učenje:", accuracy_score(y_train, y_train_p_KNN))
+
+plot_decision_regions(X_train_n, y_train, classifier=KNN_model)
+plt.xlabel('x_1')
+plt.ylabel('x_2')
+plt.legend(loc='upper left')
+plt.title("Točnost: " + "{:0.4f}".format((accuracy_score(y_train, y_train_p_KNN))))
+plt.tight_layout()
+plt.show()
+
+# Overfit za K=1, losa generalizacija za nove podatke, manja tocnost nad podacima za testiranje u odnosu na podatke za učenje
+# Underfit za K=100, niska razina točnosti, previše podataka "curi" van
+
+# Zadatak 2
+knn = KNeighborsClassifier()
+param_grid = {'n_neighbors': np.arange(1, 100)}
+knn_gscv = GridSearchCV(knn, param_grid, cv=5)
+knn_gscv.fit(X_train_n, y_train)
+
+print(knn_gscv.best_params_)
+print(knn_gscv.best_score_)
+
+'''
+KOMENTAR:
+Optimalna vrijednost je 7
+'''
+
+#Zadatak 3
+SVM_model = svm.SVC(C=1000, gamma=1, kernel='rbf')
+SVM_model.fit(X_train_n, y_train)
+
+y_test_p_SVM = SVM_model.predict(X_test_n)
+y_train_p_SVM = SVM_model.predict(X_train_n)
+
+print("Točnost (SVM) - testiranje:", accuracy_score(y_test, y_test_p_SVM))
+print("Točnost (SVM) - učenje:", accuracy_score(y_train, y_train_p_SVM))
+
+plot_decision_regions(X_train_n, y_train, classifier=SVM_model)
+plt.xlabel('x_1')
+plt.ylabel('x_2')
+plt.legend(loc='upper left')
+plt.title("Točnost: " + "{:0.4f}".format((accuracy_score(y_train, y_train_p_SVM))))
+plt.tight_layout()
+plt.show()
+
+'''
+KOMENTAR:
+Veća gamma == složenije granice odluke
+'''
+
+#Zadatak 4
+SVM_model = svm.SVC()
+param_grid = {"C": [10, 100, 100],
+              "gamma": [10, 1, 0.1, 0.01]}
+
+svm_gscv = GridSearchCV(SVM_model, param_grid, cv=5, scoring='accuracy', n_jobs=-1)
+svm_gscv.fit(X_train_n, y_train)
+
+print(svm_gscv.best_params_)
+print(svm_gscv.best_score_)
+
+'''
+KOMENTAR:
+Optimalne vrijednosti su: C = 10 i gamma = 1
+'''
 

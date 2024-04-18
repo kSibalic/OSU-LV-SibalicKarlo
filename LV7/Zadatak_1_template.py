051b2ec0862411783baa40a6ba75e0417c338e8e
kSibalic <sibalickarlo@gmail.com>
2024-04-18 09:43:32 +0200

    Upload exercises 1 & 2

--- LV7/Zadatak_1_template.py
+++ LV7/Zadatak_1_template.py
@@ -1,50 +1,68 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.cluster.hierarchy import dendrogram
 from sklearn.datasets import make_blobs, make_circles, make_moons
-from sklearn.cluster import KMeans, AgglomerativeClustering
-
+from sklearn.cluster import KMeans
 
 def generate_data(n_samples, flagc):
     # 3 grupe
     if flagc == 1:
         random_state = 365
-        X,y = make_blobs(n_samples=n_samples, random_state=random_state)
-    
+        X, y = make_blobs(n_samples=n_samples, random_state=random_state)
+
     # 3 grupe
     elif flagc == 2:
         random_state = 148
-        X,y = make_blobs(n_samples=n_samples, random_state=random_state)
+        X, y = make_blobs(n_samples=n_samples, random_state=random_state)
         transformation = [[0.60834549, -0.63667341], [-0.40887718, 0.85253229]]
         X = np.dot(X, transformation)
 
-    # 4 grupe 
+    # 4 grupe
     elif flagc == 3:
         random_state = 148
         X, y = make_blobs(n_samples=n_samples,
-                        centers = 4,
-                        cluster_std=np.array([1.0, 2.5, 0.5, 3.0]),
-                        random_state=random_state)
+                          centers=4,
+                          cluster_std=np.array([1.0, 2.5, 0.5, 3.0]),
+                          random_state=random_state)
     # 2 grupe
     elif flagc == 4:
         X, y = make_circles(n_samples=n_samples, factor=.5, noise=.05)
-    
-    # 2 grupe  
+
+    # 2 grupe
     elif flagc == 5:
         X, y = make_moons(n_samples=n_samples, noise=.05)
-    
+
     else:
         X = []
-        
+
     return X
 
 # generiranje podatkovnih primjera
-X = generate_data(500, 1)
+X = generate_data(500, 2)
 
 # prikazi primjere u obliku dijagrama rasprsenja
 plt.figure()
-plt.scatter(X[:,0],X[:,1])
+plt.scatter(X[:, 0], X[:, 1])
 plt.xlabel('$x_1$')
 plt.ylabel('$x_2$')
 plt.title('podatkovni primjeri')
 plt.show()
+
+km = KMeans(n_clusters=3, init='random')
+km.fit(X)
+labels = km.predict(X)
+centroids = km.cluster_centers_
+
+plt.figure()
+plt.scatter(X[:, 0], X[:, 1], c=labels)
+plt.scatter(centroids[:, 0], centroids[:, 1], marker='x')
+plt.xlabel('$x_1$')
+plt.ylabel('$x_2$')
+plt.title('podatkovni primjeri')
+plt.show()
+
+'''
+KOMENTAR:
+Ako za ovaj primjer pokusamo promijeniti K, na veci ili manji broj, mozemo lako zakljuciti 
+da nije bolja grupacija od one kad je K=3 (za K=2 je premalo, odnosno preveliki je razmak,
+a za K>3 rasporedi "nemaju" smisla.
+'''
--- LV7/Zadatak_2_template.py
+++ LV7/Zadatak_2_template.py
@@ -17,8 +17,65 @@ plt.show()
 img = img.astype(np.float64) / 255
 
 # transfromiraj sliku u 2D numpy polje (jedan red su RGB komponente elementa slike)
-w,h,d = img.shape
-img_array = np.reshape(img, (w*h, d))
+w, h, d = img.shape
+img_array = np.reshape(img, (w * h, d))
 
 # rezultatna slika
 img_array_aprox = img_array.copy()
+
+# a)
+unique_colors = np.unique(img_array_aprox, axis=0)
+print('Broj razlicitih boja: ', len(unique_colors))
+
+# b), c), d), e)
+km = KMeans(n_clusters=2, init='random', n_init=5, random_state=0)
+km.fit(img_array)
+
+cluster_centers = km.cluster_centers_
+labels = km.predict(img_array)
+
+img_array_aprox = cluster_centers[labels]
+img_aprox = np.reshape(img_array_aprox, (w, h, d))
+
+plt.figure()
+plt.title("Druga slika")
+plt.imshow(img_aprox)
+plt.tight_layout()
+plt.show()
+
+'''
+KOMENTAR:
+Kada je K=2 na slici mozemo vidjeti samo jednu nijansu plave i zute boje.
+K=2 je takodjer i najbolji odabir koji mozemo uzeti, sto mozemo vidjeti i potvrditi iz grafickog prikaza u zadatku f)
+'''
+
+# f)
+max_clusters = range(1, 10)
+distortions = []
+
+for k in (max_clusters):
+    kmeans = KMeans(n_clusters=k, random_state=0, n_init=10)
+    kmeans.fit(img_array)
+    distortions.append(kmeans.inertia_)
+
+plt.figure()
+plt.xlabel('Broj grupa K')
+plt.ylabel('J')
+plt.plot(max_clusters, distortions)
+plt.grid(True)
+plt.show()
+
+# g)
+labels = km.labels_
+
+for cluster_id in range(2):
+    cluster_mask = labels.reshape(w, h) == cluster_id
+
+    binary_image = np.zeros((w, h), dtype=np.uint8)
+    binary_image[cluster_mask] = 255
+
+    plt.figure()
+    plt.title("Binary Image for Cluster {}".format(cluster_id))
+    plt.imshow(binary_image, cmap='gray')
+    plt.tight_layout()
+    plt.show()
